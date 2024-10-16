efbc18148313308e18c127447d29dbea215ba675
Samuel <yusheng593@gmail.com>
2024-10-16 19:38:55 +0800

    Initial commit

---
+++ lib/screens/movies_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MoviesScreen extends StatelessWidget {
+  const MoviesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello MovieVM!'),
+      ),
+    );
+  }
+}
